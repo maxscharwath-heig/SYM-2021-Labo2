d272284a69271a2cc49278003265b1ef9e3af3a2
Maxime Scharwath <maxscharwath@gmail.com>
2021-10-12 10:00:57 +0200

    Little request txt

--- app/src/main/java/ch/heigvd/sym_labo2/AsyncActivity.kt
+++ app/src/main/java/ch/heigvd/sym_labo2/AsyncActivity.kt
@@ -2,10 +2,19 @@ package ch.heigvd.sym_labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 
 class AsyncActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        val comManager = SymComManager();
+        comManager.sendRequest("http://mobile.iict.ch/api/txt","Yolololol")
     }
 }
--- app/src/main/java/ch/heigvd/sym_labo2/SymComManager.kt
+++ app/src/main/java/ch/heigvd/sym_labo2/SymComManager.kt
@@ -1,9 +1,27 @@
 package ch.heigvd.sym_labo2
 
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+
 class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
 
     fun sendRequest(url: String, request: String) {
-		//TODO to implement
+        val data = request.toByteArray(StandardCharsets.UTF_8)
+
+        val connection = URL(url).openConnection() as HttpURLConnection
+        connection.requestMethod = "POST";
+        connection.setRequestProperty("Content-Type","text/plain")
+        connection.setRequestProperty("Content-Length",data.size.toString())
+
+        val outputStream = DataOutputStream(connection.outputStream)
+        outputStream.write(data)
+        outputStream.flush()
+        val responseReader = connection.inputStream.bufferedReader()
+        val response = responseReader.readText();
+        println(response)
+        communicationEventListener?.handleServerResponse(response)
     }
 
 }
