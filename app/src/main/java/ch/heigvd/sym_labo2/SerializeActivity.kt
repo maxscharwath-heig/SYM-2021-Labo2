bb7095e3ff9ca2844b6651575264e347cea2717a
LordTT <ferraricorneloup.teo@gmail.com>
2021-11-02 09:57:51 +0100

    working on xml

--- app/src/main/java/ch/heigvd/sym_labo2/SerializeActivity.kt
+++ app/src/main/java/ch/heigvd/sym_labo2/SerializeActivity.kt
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.Log
+import android.util.Xml
 import android.widget.Button
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.encodeToString
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import kotlinx.serialization.decodeFromString
+import android.widget.AdapterView
+
+import android.view.View
+import android.widget.AdapterView.OnItemSelectedListener
+import org.xmlpull.v1.XmlPullParser
+import java.io.StringWriter
+
+import org.xmlpull.v1.XmlSerializer
+
+
+
 
 
 class SerializeActivity : AppCompatActivity() {
@@ -22,6 +35,7 @@ class SerializeActivity : AppCompatActivity() {
     private lateinit var editData2 : EditText
     private lateinit var textResult1 : TextView
     private lateinit var textResult2 : TextView
+    private lateinit var spinner : Spinner
 
     @Serializable
     data class Data(val name: String, val surname: String)
@@ -35,30 +49,70 @@ class SerializeActivity : AppCompatActivity() {
         editData2 = findViewById(R.id.input_data2)
         textResult1 = findViewById(R.id.textView_result1)
         textResult2 = findViewById(R.id.textView_result2)
+        spinner = findViewById(R.id.spinner)
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
-        val mcm = SymComManager();
-
-
+        val mcm = SymComManager()
+        val stringArray = resources.getStringArray(R.array.data_array)
 
         sendBtn.setOnClickListener {
             var name = editData1.text.toString()
             var surname = editData2.text.toString()
-            Log.d("test",name)
-            Log.d("test",surname)
-            val json = Json.encodeToString(Data(name, surname))
+            Log.d("test", name)
+            Log.d("test", surname)
+            if (spinner.selectedItem.toString() == stringArray[0]){
 
-            mcm.setCommunicationListener(object : CommunicationEventListener {
-                override fun handleServerResponse(response: String) {
-                    val result = Json.decodeFromString<Data>(response)
-                    textResult1.text = result.name
-                    textResult2.text = result.surname
-                }
-            })
-            mcm.sendRequest("http://mobile.iict.ch/api/json", json, "application/json")
+                val json = Json.encodeToString(Data(name, surname))
 
+                mcm.setCommunicationListener(object : CommunicationEventListener {
+                    override fun handleServerResponse(response: String) {
+                        val result = Json.decodeFromString<Data>(response)
+                        textResult1.text = result.name
+                        textResult2.text = result.surname
+                    }
+                })
 
+                mcm.sendRequest("http://mobile.iict.ch/api/json", json, "application/json")
 
+            } else if(spinner.selectedItem.toString() == stringArray[1]) {
+
+                val xmlSerializer = Xml.newSerializer()
+                val xmlParser = Xml.newPullParser()
+                val writer = StringWriter()
+                xmlSerializer.setOutput(writer)
+                xmlSerializer.startDocument("UTF-8", true)
+                xmlSerializer.setFeature(
+                    "http://xmlpull.org/v1/doc/features.html#indent-output",
+                    true
+                )
+                xmlSerializer.docdecl("http://mobile.iict.ch/directory.dtd")
+                xmlSerializer.startTag("", "directory")
+                xmlSerializer.startTag("","person")
+                xmlSerializer.startTag("", "name")
+                xmlSerializer.text(name)
+                xmlSerializer.endTag("", "name")
+                xmlSerializer.startTag("", "firstname")
+                xmlSerializer.text(surname)
+                xmlSerializer.endTag("", "firstname")
+                xmlSerializer.endTag("", "person")
+                xmlSerializer.endTag("", "directory")
+                xmlSerializer.endDocument()
+
+                val toSend = writer.
+
+                mcm.setCommunicationListener(object : CommunicationEventListener {
+                    override fun handleServerResponse(response: String) {
+                       // textResult1.text = xmlParser.getAttributeValue(null, "name")
+                        // textResult2.text = xmlParser.getAttributeValue(null, "surname")
+                        textResult2.text = response
+                    }
+                })
+
+                mcm.sendRequest("http://mobile.iict.ch/api/xml", toSend , "application/xml")
+
+            } else if (spinner.selectedItem.toString() == stringArray[2]){
+
+            }
         }
     }
 }
