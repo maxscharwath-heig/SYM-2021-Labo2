68f34cee8c3d9a59ecc56e721f5050040fdd5696
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-11-10 16:23:45 +0100

    Compress activity done, refactor SymComManager

--- app/src/main/java/ch/heigvd/sym_labo2/CompressActivity.kt
+++ app/src/main/java/ch/heigvd/sym_labo2/CompressActivity.kt
@@ -2,10 +2,43 @@ package ch.heigvd.sym_labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.nio.charset.StandardCharsets
+import java.util.zip.DeflaterOutputStream
 
 class CompressActivity : AppCompatActivity() {
+
+    private lateinit var sendButton: Button
+    private lateinit var requestContentTextView: TextView
+    private lateinit var requestResultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compress)
+
+        sendButton = findViewById(R.id.send_button)
+        requestContentTextView = findViewById(R.id.request_content_text)
+        requestResultTextView = findViewById(R.id.request_result_text)
+
+        val mcm = SymComManager()
+        mcm.setCommunicationListener(object : CommunicationEventListener {
+            override fun handleServerResponse(response: String) {
+                // TODO: deflate with InflaterInputStream
+                requestResultTextView.text = response
+            }
+        })
+
+        sendButton.setOnClickListener {
+            val content = requestContentTextView.text.toString()
+
+            if (content.isBlank()) {
+                Toast.makeText(applicationContext, getString(R.string.blank_input), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            mcm.sendRequest("http://mobile.iict.ch/api/txt", content, "text/plain", true)
+        }
     }
 }
